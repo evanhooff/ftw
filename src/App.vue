af1b1f72ca4395b0289af9435551ec5585dd1476
Eline van Hooff <eline.vanhooff@isaac.nl>
2019-04-15 10:17:29 +0200

    added wulffhead

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!-- <navigation></navigation> -->
     <router-view />
   </div>
 </template>
 
+<script>
+// import navigation from "@/components/nav";
+
+// export default {
+//   components: {
+//     navigation
+//   }
+// };
+</script>
+
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  padding: 0;
+  margin: 0;
 }
 #nav {
   padding: 30px;
